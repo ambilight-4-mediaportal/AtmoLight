ccef221e83d4961a2e793562dd67fa20aa5bd250
Lightning303 <Lightning303@gmail.com>
2014-12-13 20:58:57 +0100

    Removed redundant code.

--- AtmoLight.Core/Targets/AtmoOrb/AtmoOrbHandler.cs
+++ AtmoLight.Core/Targets/AtmoOrb/AtmoOrbHandler.cs
@@ -299,11 +299,11 @@ namespace AtmoLight.Targets
                   double lightnessOverall;
 
                   // Convert to hsl color model
-                  RGB2HSL(lamp.AverageColor[0], lamp.AverageColor[1], lamp.AverageColor[2], out hue, out saturation, out lightness);
-                  RGB2HSL(lamp.OverallAverageColor[0], lamp.OverallAverageColor[1], lamp.OverallAverageColor[2], out hueOverall, out saturationOverall, out lightnessOverall);
+                  HSL.RGB2HSL(lamp.AverageColor[0], lamp.AverageColor[1], lamp.AverageColor[2], out hue, out saturation, out lightness);
+                  HSL.RGB2HSL(lamp.OverallAverageColor[0], lamp.OverallAverageColor[1], lamp.OverallAverageColor[2], out hueOverall, out saturationOverall, out lightnessOverall);
 
                   // Convert back to rgb with adjusted saturation and lightness
-                  HSL2RGB(hue, Math.Min(saturation + coreObject.atmoOrbSaturation, 1), (coreObject.atmoOrbUseOverallLightness ? lightnessOverall : lightness), out lamp.AverageColor[0], out lamp.AverageColor[1], out lamp.AverageColor[2]);
+                  HSL.HSL2RGB(hue, Math.Min(saturation + coreObject.atmoOrbSaturation, 1), (coreObject.atmoOrbUseOverallLightness ? lightnessOverall : lightness), out lamp.AverageColor[0], out lamp.AverageColor[1], out lamp.AverageColor[2]);
 
                   // Save new color as previous color
                   lamp.PreviousColor = lamp.AverageColor;
@@ -421,128 +421,6 @@ namespace AtmoLight.Targets
       {
         gammaCurve[i] = Math.Pow((double)i / ((double)gammaCurve.Length - 1.0f), coreObject.atmoOrbGamma) * (gammaCurve.Length - 1.0f);
       }
-    }
-
-    // RGB2HSL and HSL2RGB
-    //
-    // http://www.geekymonkey.com/Programming/CSharp/RGB2HSL_HSL2RGB.htm
-
-    // Given H,S,L in range of 0-1
-    // Returns a Color (RGB struct) in range of 0-255
-    public void HSL2RGB(double h, double sl, double l, out int red, out int green, out int blue)
-    {
-      double v;
-      double r, g, b;
-
-      r = l;   // default to gray
-      g = l;
-      b = l;
-      v = (l <= 0.5) ? (l * (1.0 + sl)) : (l + sl - l * sl);
-      if (v > 0)
-      {
-        double m;
-        double sv;
-        int sextant;
-        double fract, vsf, mid1, mid2;
-
-        m = l + l - v;
-        sv = (v - m) / v;
-        h *= 6.0;
-        sextant = (int)h;
-        fract = h - sextant;
-        vsf = v * sv * fract;
-        mid1 = m + vsf;
-        mid2 = v - vsf;
-        switch (sextant)
-        {
-          case 0:
-            r = v;
-            g = mid1;
-            b = m;
-            break;
-          case 1:
-            r = mid2;
-            g = v;
-            b = m;
-            break;
-          case 2:
-            r = m;
-            g = v;
-            b = mid1;
-            break;
-          case 3:
-            r = m;
-            g = mid2;
-            b = v;
-            break;
-          case 4:
-            r = mid1;
-            g = m;
-            b = v;
-            break;
-          case 5:
-            r = v;
-            g = m;
-            b = mid2;
-            break;
-        }
-      }
-      red = Convert.ToByte(r * 255.0f);
-      green = Convert.ToByte(g * 255.0f);
-      blue = Convert.ToByte(b * 255.0f);
-    }
-
-
-    // Given a Color (RGB Struct) in range of 0-255
-    // Return H,S,L in range of 0-1
-    public void RGB2HSL(int red, int green, int blue, out double h, out double s, out double l)
-    {
-      double r = red / 255.0;
-      double g = green / 255.0;
-      double b = blue / 255.0;
-      double v;
-      double m;
-      double vm;
-      double r2, g2, b2;
-
-      h = 0; // default to black
-      s = 0;
-      l = 0;
-      v = Math.Max(r, g);
-      v = Math.Max(v, b);
-      m = Math.Min(r, g);
-      m = Math.Min(m, b);
-      l = (m + v) / 2.0;
-      if (l <= 0.0)
-      {
-        return;
-      }
-      vm = v - m;
-      s = vm;
-      if (s > 0.0)
-      {
-        s /= (l <= 0.5) ? (v + m) : (2.0 - v - m);
-      }
-      else
-      {
-        return;
-      }
-      r2 = (v - r) / vm;
-      g2 = (v - g) / vm;
-      b2 = (v - b) / vm;
-      if (r == v)
-      {
-        h = (g == m ? 5.0 + b2 : 1.0 - g2);
-      }
-      else if (g == v)
-      {
-        h = (b == m ? 1.0 + r2 : 3.0 - b2);
-      }
-      else
-      {
-        h = (r == m ? 3.0 + g2 : 5.0 - r2);
-      }
-      h /= 6.0;
     }
     #endregion
   }
