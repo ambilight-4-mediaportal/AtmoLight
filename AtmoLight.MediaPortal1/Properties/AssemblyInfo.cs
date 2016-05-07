63567af1927953459ae0619a1babcfb7559310f7
RickDB <RickDB@gmail.com>
2016-05-07 15:35:34 +0200

    Updated changelog.txt Bumped MP1 version to 1.18.0.0.

--- AtmoLight.MediaPortal1/Properties/AssemblyInfo.cs
+++ AtmoLight.MediaPortal1/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using MediaPortal.Common.Utils;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.17.0.10")]
-[assembly: AssemblyFileVersion("1.17.0.10")]
+[assembly: AssemblyVersion("1.18.0.0")]
+[assembly: AssemblyFileVersion("1.18.0.0")]
